9810c0b9da668e2baa11fbfcff7f1668ea6c497e
Regulus343 <codyjassman@gmail.com>
2013-02-22 18:41:27 -0700

    Package created

---
+++ src/Regulus/Exterminator/ExterminatorServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Regulus\Exterminator;
+
+use Illuminate\Support\ServiceProvider;
+
+class ExterminatorServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('regulus/exterminator');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
