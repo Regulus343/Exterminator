1111991ee7be21088cb23ddf7e90ac902d57d228
Regulus343 <codyjassman@gmail.com>
2013-02-25 23:06:12 -0700

    Many improvements and new features

--- src/Regulus/Exterminator/Exterminator.php
+++ src/Regulus/Exterminator/Exterminator.php
@@ -17,6 +17,11 @@ use Illuminate\Support\Facades\URL;
 
 class Exterminator {
 
+	public static $shownCss = false;
+	public static $shownJs = false;
+
+	public static $debugData = array();
+
 	/**
 	 * Dumps data to the screen and exits.
 	 *
@@ -30,9 +35,7 @@ class Exterminator {
 			return Redirect::to($uri)->withCookie(Cookie::make('debug', true, 10800));
 		}
 
-		if (Cookie::get('debug')) {
-			return true;
-		}
+		if (Cookie::get('debug')) return true;
 		return false;
 	}
 
@@ -54,6 +57,15 @@ class Exterminator {
 	}
 
 	/**
+	 * Used to enable Exterminator by running it with a predefined string.
+	 *
+	 */
+	public static function e()
+	{
+		return static::x('Exterminator enabled!');
+	}
+
+	/**
 	 * Dumps data to the screen.
 	 *
 	 * @param  mixed    $data
@@ -64,7 +76,6 @@ class Exterminator {
 		if (!is_bool($authorized)) return $authorized;
 
 		if ($authorized) static::dump($data);
-		return;
 	}
 
 	/**
@@ -74,25 +85,135 @@ class Exterminator {
 	 */
 	private static function dump($data)
 	{
-		echo '<style type="text/css">';
-		echo 'div.debug { z-index: 10800; position: absolute; top: 120px; right: 24px; }';
-		echo 'div.debug div.debug-bg { position: absolute; opacity: 0.7; width: 100%; height: 100%; background-color: #000;';
-		echo '-moz-border-radius: 8px; -webkit-border-radius: 8px; border-radius: 8px; }';
-		echo 'div.debug:hover div.debug-bg { opacity: 1; }';
-		echo 'div.debug pre { position: relative; padding: 16px; background: none; color: #fff; }';
-		echo '</style>';
-		echo '<div class="debug"><div class="debug-bg"></div><pre>';
+		echo static::css();
+		echo static::openPanel();
+
+		echo '<pre>';
 		var_dump($data);
-		echo '</pre></div>';
+		echo '</pre></div>' . "\n";
 	}
 
 	/**
-	 * Used to enable Exterminator by running it with a predefined string.
+	 * Adds debug data to debug data array
+	 *
+	 * @param  mixed    $data
+	 */
+	public static function a($data)
+	{
+		static::$debugData[] = $data;
+	}
+
+	/**
+	 * Prepares debug data for display.
 	 *
 	 */
-	public static function e()
+	public static function display()
 	{
-		return static::x('Exterminator enabled!');
+		$authorized = static::authorized();
+		if (!is_bool($authorized)) return $authorized;
+
+		if ($authorized && !empty(static::$debugData)) {
+			echo static::css();
+			echo static::openPanel();
+			echo '<a href="" class="toggle-debug">Hide</a>' . "\n";
+			foreach (static::$debugData as $data) {
+				echo '<pre>' . "\n";
+				echo static::varDump($data);
+				echo '</pre>' . "\n";
+			}
+			echo '</div>' . "\n";
+			echo static::js();
+		}
+	}
+
+	/**
+	 * Gets a string from a var_dump() and formats HTML special characters so that strings show exact data
+	 * and no data erroneously renders HTML tags.
+	 *
+	 * @param  mixed   $var
+	 *
+	 */
+	public static function varDump($var = false)
+	{
+		ob_start();
+    	var_dump($var);
+    	$string = ob_get_clean();
+    	return static::entities($string);
+	}
+
+	/**
+	 * Creates the debug panel opening markup.
+	 *
+	 * @return string
+	 */
+	private static function openPanel()
+	{
+		$html = '<div class="debug"><div class="debug-bg"></div>' . "\n";
+		$html .= '<h1>Exterminator</h1>' . "\n";
+		return $html;
+	}
+
+	/**
+	 * Creates the CSS for the debug panel.
+	 *
+	 * @return string
+	 */
+	private static function css()
+	{
+		if (!static::$shownCss) {
+			$html = '<style type="text/css">' . "\n";;
+			$html .= 'div.debug { z-index: 10800; position: fixed; top: 80px; right: 36px; min-width: 560px; max-width: 980px; min-height: 24px;';
+			$html .= 'font-family: Arial, Helvetica, sans-serif; }';
+			$html .= 'div.debug div.debug-bg { position: absolute; opacity: 0.9; width: 100%; height: 100%; background-color: #000;';
+			$html .= '-moz-border-radius: 8px; -webkit-border-radius: 8px; border-radius: 8px; }';
+			$html .= 'div.debug:hover div.debug-bg { opacity: 0.97; }';
+			$html .= 'div.debug a.toggle-debug { position: absolute; top: -14px; right: 8px; padding: 2px 3px; ';
+			$html .= 'background-color: #700; color: #fff; font-size: 11px; font-weight: bold; text-decoration: none; cursor: pointer; ';
+			$html .= 'border: 1px solid #888; -moz-border-radius: 4px; -webkit-border-radius: 4px; border-radius: 4px; }';
+			$html .= 'div.debug a.toggle-debug.show { background-color: #060; }';
+			$html .= 'div.debug a.toggle-debug:hover { background-color: #fff; color: #000; }';
+			$html .= 'div.debug h1 { position: absolute; top: -8px; left: 0; background: none; color: #700; ';
+			$html .= 'font-family: Arial, Helvetica, sans-serif; font-size: 18px; font-weight: normal; font-style: italic; ';
+			$html .= 'text-shadow: #000 0 0 5px; }';
+			$html .= 'div.debug pre { position: relative; margin: 12px 6px; padding: 10px; ';
+			$html .= 'background: none; border: 1px solid #555; color: #fff; }';
+			$html .= '</style>' . "\n";
+			return $html;
+		}
+	}
+
+	/**
+	 * Creates the CSS for the debug panel.
+	 *
+	 * @return string
+	 */
+	private static function js()
+	{
+		if (!static::$shownJs) {
+			$html = '<script type="text/javascript">' . "\n";
+			$html .= '$("a.toggle-debug").click(function(e){' . "\n";
+			$html .= 'e.preventDefault();' . "\n";
+			$html .= 'if ($(this).text() == "Hide") {' . "\n";
+			$html .= '$(this).parents("div.debug").children("pre").fadeOut("fast"); $(this).addClass("show").text("Show");' . "\n";
+			$html .= '} else { $(this).parents("div.debug").children("pre").fadeIn("fast"); ';
+			$html .= '$(this).removeClass("show").text("Hide"); }' . "\n";
+			$html .= '});' . "\n";
+			$html .= '</script>' . "\n";
+			return $html;
+		}
+	}
+
+	/**
+	 * Convert HTML characters to entities.
+	 *
+	 * The encoding specified in the application configuration file will be used.
+	 *
+	 * @param  string  $value
+	 * @return string
+	 */
+	public static function entities($value)
+	{
+		return htmlentities($value, ENT_QUOTES, 'UTF-8', false);
 	}
 
 }
